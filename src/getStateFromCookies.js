519b303fd9086d55a02f2176d445153052b95b97
Lucas Stennett <lucas.stennett@parivedasolutions.com>
2017-08-13 18:00:40 -0400

    remove path slicer in favor of lodash functionality

--- src/getStateFromCookies.js
+++ src/getStateFromCookies.js
@@ -1,23 +1,6 @@
 import { getCookie as getBrowserCookie } from './cookieApi';
 import _get from 'lodash/get';
-
-/**
- * return the node referenced by path in state.
- * @param {Object} path
- * @return {Object} node reference
- **/
-function pathSlicer(path) {
-    const getSubtree = (subtree, key) => {
-        if (key.indexOf('.') > -1) {
-            const remaining = key.split('.').slice(1).join('.');
-
-            return getSubtree(subtree[key.split('.')[0]], remaining);
-        }
-        return subtree[key];
-    };
-
-    return (state) => getSubtree(state, path);
-}
+import _set from 'lodash/set';
 
 /**
  * read browser cookie into state
@@ -33,8 +16,6 @@ const getStateFromCookies = (
 ) => {
     Object.keys(paths).forEach(pathToState => {
         const pathConf = paths[pathToState];
-        const pathSplit = pathToState.split('.');
-        const terminalKey = pathSplit.slice(-1);
 
         // read cookies
         let storedState = JSON.parse(getCookie(pathConf.name));
@@ -43,16 +24,8 @@ const getStateFromCookies = (
         }
         debugger;
         // get a slice of state path where to put cookie value
-        const stateTree = pathSplit.length > 1 ? (
-            pathSlicer(pathSplit.slice(0, -1).join('.'))(preloadedState)
-        ) : preloadedState;
-
         if (storedState) {
-            try {
-                stateTree[terminalKey] = JSON.parse(storedState);
-            } catch (err) {
-                console.error(`Unable to set state from cookie at ${pathConf.name}. Error: `, err);
-            }
+            _set(preloadedState, pathToState, storedState);
         }
     });
 
